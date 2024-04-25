7317b06b19fd314ba2699c08e3ba7f75487365fa
Lucie-Baborova <lucie.babor@gmail.com>
2024-04-26 00:04:15 +0200

    making data available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {COURSES} from '../db-data';
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,24 @@ import {COURSES} from '../db-data';
 })
 export class AppComponent {
 
+  courses = COURSES;
 
+  startDate = new Date(2000, 0, 1);
+
+  title = COURSES[1].description;
+
+  price = 9.9987578587;
+
+  rate = 0.67;
+
+  course = COURSES[1];
+
+  onCourseSelected(course: Course) {
+    console.log("card-component - click event bubbled...", course)
+  }
+
+  // trackCourse(index:number, course:Course) {
+  //   return course.id;
+  // }
 
 }
